72c88b551daab75014d34d8501e7016956ab5ba6
Umur Gedik <umurgdk@gmail.com>
2024-11-30 16:28:18 +0300

    fix: alacritty compatibility with macos

--- lightswitch.sh
+++ lightswitch.sh
@@ -1,18 +1,22 @@
 switch_lights()
 {
 	scheme=$1
-	case "$scheme" in
-		dark)
-			echo dark > ~/.interface_style
-			ln -sf $HOME/.config/alacritty/dark.toml /home/umurgdk/.config/alacritty/alacritty.toml
-			plasma-apply-colorscheme BreezeDark >/dev/null
-			exit;;
-		light)
-			echo light > ~/.interface_style
-			ln -sf $HOME/.config/alacritty/light.toml /home/umurgdk/.config/alacritty/alacritty.toml
-			plasma-apply-colorscheme BreezeLight >/dev/null
-			exit;;
+	alacritty_config="${scheme}.toml"
+	case "$(uname -s)" in
+		Darwin*)
+			alacritty_config="${scheme}_macos.toml";;
+		*)
+			case "$scheme" in
+				dark)
+					plasma-apply-colorscheme BreezeDark >/dev/null;;
+				light)
+					plasma-apply-colorscheme BreezeLight >/dev/null;;
+			esac;;
 	esac
+
+	echo "alacritty_config=${alacritty_config}"
+	echo "$scheme" > ~/.interface_style
+	ln -sf $HOME/.config/alacritty/${alacritty_config} $HOME/.config/alacritty/alacritty.toml
 }
 
 if [ -z "$1" ]; then
